f3b2b4cfed0263b9fe7ef3784e2707c27bf44cd3
Matt Smitherman <mtsmithe@uncg.edu>
2018-03-20 18:59:36 -0400

    created gradAss class, modified print functions

--- CSC_339_Proj1/mtsmithe_Undergrad.cpp
+++ CSC_339_Proj1/mtsmithe_Undergrad.cpp
@@ -9,9 +9,9 @@ This program defines a parent class: Student, and three subclasses: Undergrad, G
 implements STL and i/o functions to write to files, and defines a sort function (overloading "<" operator)
 */
 
-#include<iostream>;
-#include<string.h>;
-#include<stdio.h>;
+#include<iostream>
+#include<string.h>
+#include<stdio.h>
 using namespace std;
 
 class Student {
@@ -36,7 +36,10 @@ class Student {
 
 		};
 
-		virtual void print() {}
+		virtual void print() {
+			
+			cout << name << "	" << ssn << "	 " << gpa << "	 " << credits;
+		}
 		virtual float tuition() = 0;
 
 		void set_GPA(int gpa) {
@@ -46,13 +49,18 @@ class Student {
 		void set_Credits(int credits) {
 			this-> credits = credits;
 		}
+		void display_Head() {
+			cout << "Name:	" << "SSN:		" << "GPA:	" << "Credits:   "
+				<< "Year/Title:	" << "	Tuition:	" << "Thesis:		"
+				<< "Hourly Pay:	" << "Supervisor:" << "Task:" << endl;
+		}
 
 };
 
 class Mtsmithe_Undergrad : public Student {
 
 	protected:
-		float undergrad_rate;
+		float undergrad_rate = 380.0;
 		char yearArray[10];
 		char (*year)[10] = &yearArray;
 		
@@ -75,13 +83,12 @@ class Mtsmithe_Undergrad : public Student {
 
 
 		float tuition() {
-			undergrad_rate = 380.0;
 			return undergrad_rate;
 		};
 		void print() {
-			cout << "Name: " << name << " SSN: " << ssn << " Year: " << *year
-				<< " credits: " + credits  << " tuition rate: $" << undergrad_rate 
-				<< " " << "GPA: " << gpa << endl;
+			cout << endl;
+			Student::print();
+			cout << "       " << *year << "		" << undergrad_rate;
 		}
 
 };
@@ -92,13 +99,16 @@ class Grad : public Student {
 		char thesisArray[70];
 		char (*thesis)[70] = &thesisArray;
 		char *thesisPtr;
+		char titleArr[13];
+		char(*title)[13] = &titleArr;
 
 	public:
-		Grad(const char name[21], const char ssn[10], float gpa, int credits, 
-			char parThesis[]) : Student(name, ssn, gpa, credits){
+		Grad(const char name[21], const char ssn[10], float gpa, int credits,
+			char parThesis[], char parTitle[]) : Student(name, ssn, gpa, credits) {
 
 			tuition();
 			strcpy_s(*thesis, sizeof thesisArray, parThesis);
+			strcpy_s(*title, sizeof titleArr, parTitle);
 				
 		};
 
@@ -114,13 +124,12 @@ class Grad : public Student {
 			return thesisPtr;
 		}
 		void set_thesis(char * newPointer) {
-			delete thesisArray;
 			thesisPtr = newPointer;
 		}
 		void print() {
-			cout << "Name: " << name << " SSN: " << ssn << " Year: graduate student"
-				<< " credits: " + credits << " tuition rate: $" << grad_rate
-				<< " GPA: " << gpa << " Thesis: " << *thesis << endl;
+			cout << endl;
+			Student::print();
+			cout << "	  graduate " << *title << "	" << grad_rate << "		 " << *thesis;
 		}
 
 };
@@ -130,27 +139,64 @@ class GradAsst : public Grad {
 		float hourPay;
 		char * supervisor;
 		char * task;
+		char titleArr[13];
+		char(*title)[13] = &titleArr;
+
 	public:
 		GradAsst(const char name[21], const char ssn[10], float gpa, int credits,
-			char parThesis[]) : Grad(name, ssn, gpa, credits, parThesis) {
-			/*
-			*supervisorPtr = "Dr.Fu";
-			set_hourPay(20.0);
-			set_supervisor();
-			*/
+			char parThesis[], float hourPay, char supervisor[21], char task[40], char parTitle[])
+				: Grad(name, ssn, gpa, credits, parThesis, parTitle), hourPay(hourPay),
+					supervisor(supervisor), task(task) {
+
+			strcpy_s(*title, sizeof titleArr, parTitle);
 		};
+
+		char * get_supervisor() {
+			return supervisor;
+		}
+
+		float get_hourPay() {
+			return hourPay;
+		}
+
+		void set_supervisor(char * mySupervisor) {
+			delete supervisor;
+			supervisor = mySupervisor;
+		}
+
+		void set_hourPay(float pay) {
+			hourPay = pay;
+		}
+
+		char * get_task() {
+			return task;
+		}
+
+		void set_task(char * myTask) {
+			delete task;
+			task = myTask;
+		}
+
+		void print() {
+			Grad::print();
+			cout << "	"<<  hourPay << "   " << supervisor << "   " << task << endl;
+		}
 };
 
 
 //main function executes program
 int main() {
 
+
 	Mtsmithe_Undergrad student("Mary", "000111222", 4.0, 12, "Junior");
+	student.display_Head();
 	student.print();
 	
-	Grad gStudent("David", "111222333", 3.7, 9, "How to learn data structures using C++/STL");
+	Grad gStudent("David", "111222333", 3.7, 9, "How to learn data structures using C++/STL", "student");
 	gStudent.print();
 
+	GradAsst gradAss("Jason", "222333444", 3.7, 9, "Design of efficient algorithms", 20.0, "Dr. Fu", "Grading 330 projects and implementing a data mining algorithm", "assistant");
+	gradAss.print();
 
 	return 0;
 }
